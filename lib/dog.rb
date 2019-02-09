65ed431d6757e79ecfaf5c8a588fafe8364aa11e
Kathleen Carrel <katiecarrel@gmail.com>
2019-02-09 22:01:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,6 +11,21 @@ class Dog
   def create_table 
   end 
   
+  def drop_table 
+  end 
+  
+  def new_from_db 
+  end 
+  
+  def find_by_name 
+  end 
+  
+  def update 
+  end 
+  
+  def save 
+  end 
+  
   
   
 end 
